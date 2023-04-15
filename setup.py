62d40ad0b128182b6f38a361722a19456a24a4c6
NextWordDev <public@nextword.dev>
2023-04-14 22:45:59 -0400

    fix tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='psychoevals',
-    version='0.4',
+    version='0.6',
     packages=find_packages(),
     install_requires=[
         "openai>=0.27.0",
